9e5ac93546fb52cb1ba06812885e2f3064ef316c
Simon Brewer <brewersi@gmail.com>
2021-10-10 15:10:31 -0600

    Added noise, and scaling to SPDF (plus reasonable estimates of r and sigma)

--- 02_make_spdf.R
+++ 02_make_spdf.R
@@ -1,8 +1,11 @@
 ## Derive a population curve from the Trace data
+set.seed(42)
+source("00_functions.R")
 
 dat <- read.csv("./data/AGU_Bonneville_cSPD_T21KClim.csv")
 
-dat <- subset(dat, CalBP <= 2500)
+## Start poopn growth at 3000
+dat <- subset(dat, CalBP <= 3000)
 
 plot(dat$CalBP, dat$AT, type ='l')
 
@@ -25,6 +28,18 @@ pred_K <- sapply(st, clim_k_mod, min_clim, max_clim, min_K, max_K)
 plot(st, pred_K, type ='l')
 
 ## Now get set of `K` for the sytrace data
-pred_K <- sapply(dat$AT, clim_k_mod, min_clim, max_clim, min_K, max_K)
+dat$pred_K <- sapply(dat$AT, clim_k_mod, min_clim, max_clim, min_K, max_K)
+dat$t <- 1:nrow(dat)
 
+dat$test_K <- 500
+mysigma <- 5.5 ## Noise term
+dat$P <- log_mod_dyn(dat$t, 2, r = 0.15, dat$pred_K, 
+                     noise = TRUE, mysigma)
 
+plot(dat$t, dat$P)
+
+## Constant scaling to SPDF
+spdf_fac <- 0.001 ## 0.1% of people leave a trace
+dat$spdf <- dat$P * spdf_fac
+plot(dat$t, dat$spdf)
+# plot(dat$t, dat$AT)
