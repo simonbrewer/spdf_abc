2cb51dc3befdb3c584e2a7a3c3528343e8f54790
Simon Brewer <brewersi@gmail.com>
2021-10-10 14:50:46 -0600

    Fixed log model

--- 00_functions.R
+++ 00_functions.R
@@ -21,9 +21,25 @@ clim_k_mod <- function(clim, min_clim, max_clim, min_K, max_K)
   return(K)
 }
 
-log_mod <- function(t, X0, r, K) {
-  # X <- (K*X0) / ( X0-(X0-K)*exp(r*t)) ## From SEEM book
+log_mod_fixed <- function(t, X0, r, K) {
+  
   X <- (K*X0) / ( X0+(K-X0)*exp(-r*t)) ## From maa.org
-  # X <- 1 / (1 + (1/X0 -  1) * exp(-r*t)) ## From Wolfram
+  
   return(data.frame(t,X))
+}
+
+log_mod_dyn <- function(t, X0, r, K) {
+  
+  if (length(K) < 2) {stop("K is a single value")}
+  
+  nt <- length(t)
+  X <- rep(NA, nt) 
+  X[1] <- X0
+  
+  for (i in 2:nt) {
+    dX <- r * (1 - (X[(i-1)] / K[i])) * X[(i - 1)]
+    X[i] <- X[(i-1)] + dX
+  }
+
+  return(X)
 }
